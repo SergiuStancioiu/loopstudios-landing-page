335b6149e41eb5da0c6b86b18b3eca4fcf43f47d
Sergiu Stancioiu <s.sergiu96@gmail.com>
2026-02-28 20:11:48 +0200

    Added functionality for mobile menu

--- js/our-creations.js
+++ js/our-creations.js
@@ -40,3 +40,20 @@ function creationCard(creation) {
 
   container.appendChild(clone);
 }
+function showMobileNavLinks() {
+  const navLinkButton = document.querySelector('.hamburger-container');
+  const navLinksMeniu = document.querySelector('.mobile-navbar');
+  const mobileCloseMenu = document.querySelector('.mobile-close-menu');
+
+  navLinkButton.addEventListener('click', () => {
+    navLinksMeniu.classList.toggle('show-mobile-links');
+    document.body.classList.toggle('hideSidebar');
+  });
+
+  mobileCloseMenu.addEventListener('click', () => {
+    navLinksMeniu.classList.remove('show-mobile-links');
+    document.body.classList.remove('hideSidebar');
+  });
+}
+
+showMobileNavLinks();
